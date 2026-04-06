0e7766b734cca4d16b87acd672a252595a6705f4
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-04-06 21:32:47 +0700

    update survey.php

---
+++ Tugas Praktikum 3/Sistem Klinik Sehat/proses/survey.php
@@ -0,0 +1,23 @@
+<?php
+include '../server/koneksi.php';
+
+// Ambil data dari form
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+$umur = $_POST['umur'];
+$jenis_kelamin = $_POST['jenis_kelamin'];
+$komentar = $_POST['komentar'];
+
+// Query insert
+$query = "INSERT INTO survey (nama, email, umur, jenis_kelamin, komentar)
+          VALUES ('$nama', '$email', '$umur', '$jenis_kelamin', '$komentar')";
+
+// Eksekusi
+if (mysqli_query($conn, $query)) {
+    echo "Data survey berhasil disimpan!";
+    // redirect kalau mau:
+    // header("Location: dashboard.php");
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+?>
