5a8d130b242f7e10395ab03fd18a9e5b74fae21b
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-04-06 15:40:14 +0700

    update login.php

---
+++ Tugas Praktikum 3/Sistem Klinik Sehat/proses/login.php
@@ -0,0 +1,15 @@
+<?php
+include 'koneksi.php';
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$result = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    header("Location: dashboard.php");
+} else {
+    echo "Login gagal";
+}
+?>
