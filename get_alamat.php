cdbce255314af2c1bd491f61610b8d13dccc5777
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-04-16 12:12:06 +0700

    update get_alamat.php

--- get_alamat.php
+++ get_alamat.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json'); 
 
 // URL API BPS spesifik yang sudah dites di Postman
-$url = "https://webapi.63cd767ef7a24cd0782b015036fd6414/";
+$url = "https://webapi.bps.go.id/v1/api/domain/type/all/prov/00000/key/52633aaa635a98a9631e14167a1a9fd4/";
 
 // ambil data
 $response = file_get_contents($url);
